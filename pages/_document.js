db69af681ca6fb2f0081d21f9053aa55ec5cf834
Abolfazl-ghodrati-k <104020673+Abolfazl-ghodrati-k@users.noreply.github.com>
2023-03-13 23:40:51 +0330

    app release v1.0.0

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="icon" href="/Images/favicon.ico" />
         <link rel="preconnect" href="//fdn.fontcdn.ir" />
         <link rel="preconnect" href="//v1.fontapi.ir" />
         <link href="https://v1.fontapi.ir/css/Vazir" rel="stylesheet"></link>
